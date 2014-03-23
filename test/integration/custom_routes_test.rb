bc29fc9288e6053655118a16a070926cecaa8567
Binh Nguyen <rbtnguyen@gmail.com>
2014-03-23 16:28:35 -0700

    Updated profile and added scoped status updates

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -20,4 +20,9 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
   	get '/register'
   	assert_response :success
   end
+
+  test "that a profile page works" do
+    get '/jeffyguy'
+    assert_response :success
+  end
 end
